9c7402ca0ac31e8f675ec2443be51f3fee0bc5a6
Filip <filip.makos49@gmail.com>
2025-04-27 17:41:14 +0200

    feat: fps counter

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,26 @@
 #include <SDL3/SDL.h>
 
 // Own libraries
+#include "bismuth/registry.hpp"
+#include "quartz/core/components/camera_component.hpp"
+#include "quartz/core/components/transform_component.hpp"
+#include "quartz/core/systems/camera_system.hpp"
 #include "quartz/engine.hpp"
+#include "quartz/graphics/instanced_renderer_system.hpp"
 
 quartz::Engine gEngine;
+bismuth::Registry gRegistry;
+
+void InitEntities() {
+    size_t entity = gRegistry.CreateEntity();
+    CameraComponent camera;
+    TransformComponent transform;
+
+    camera.aspectRatio = 640.0f/480.0f;
+
+    gRegistry.EmplaceComponent<CameraComponent>(entity, camera);
+    gRegistry.EmplaceComponent<TransformComponent>(entity, transform);
+}
 
 void Event(float deltaTime) {
     SDL_Event e;
@@ -21,17 +38,31 @@ void Event(float deltaTime) {
 }
 
 void System(float deltaTime) {
+    static quartz::CameraSystem cameraSystem;
+    static quartz::InstancedRendererSystem instanceRenderer;
 
+    cameraSystem.Update(gRegistry);
+    instanceRenderer.Update(gRegistry);
+}
+
+void FpsCounter(float deltaTime) {
+    static float smoothedFPS = 0.0f;
+    static float alpha = 0.1f;  
+
+    float fps = 1.0f/deltaTime;
+    smoothedFPS = alpha * fps + (1.0f - alpha) * smoothedFPS;
+    std::cout << "\rFPS: " << static_cast<int>(smoothedFPS) << std::flush;
 }
 
 void Loop(float deltaTime) {
-
+    FpsCounter(deltaTime);
 }
 
 int main(int argc, char* argv[]) {
-
+    InitEntities();
+    
     gEngine.Initialize("./config/config.json");
-
+    
     gEngine.SetEventCallback(Event);
     gEngine.SetSystemCallback(System);
     gEngine.SetUpdateCallback(Loop);
