6cecf9afb3d07ced529702c43908d2aab3129015
Filip <filip.makos49@gmail.com>
2025-05-26 18:58:16 +0200

    chore: added comment for better clarity

--- include/sapphire/utility/config.hpp
+++ include/sapphire/utility/config.hpp
@@ -5,6 +5,7 @@ namespace sapphire_config {
     constexpr static float INITIAL_SPACING = 0.5f;
     constexpr static float SMOOTHING_LENGTH = 4.0f;
 
+    // Pressure
     constexpr float REST_DENSITY = 0.6f;
     constexpr float STIFFNESS = 100.0f;
 }
