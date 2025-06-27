6ef34527c35432afd783cfd5bc572095caa724ed
Filip <filip.makos49@gmail.com>
2025-06-27 23:34:05 +0200

    feat: gui object

---
+++ include/quartz/ui/children.hpp
@@ -0,0 +1,10 @@
+#pragma once
+// C++ standard libraries
+#include <vector>
+
+// Own libraries
+#include "quartz/ui/object.hpp"
+
+struct Children {
+    std::vector<Object*> child;
+};
---
+++ include/quartz/ui/object.hpp
@@ -0,0 +1,9 @@
+#pragma once
+// Own libraries
+#include "quartz/ui/style.hpp"
+#include "quartz/ui/children.hpp"
+
+struct Object {
+    Style style;
+    Children children;
+};
