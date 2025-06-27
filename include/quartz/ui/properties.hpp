a3b062586fd8554903c5f410159f2cab2168a056
Filip <filip.makos49@gmail.com>
2025-06-27 23:26:27 +0200

    feat: style class

---
+++ include/quartz/ui/properties.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+enum class Properties {
+    width,
+    height,
+
+    position,
+
+    background_color,
+    color,
+
+    margin,
+    margin_left,
+    margin_right,
+    margin_top,
+    margin_bottom,
+
+    padding,
+    padding_left,
+    padding_right,
+    padding_top,
+    padding_bottom,
+
+    border,
+    border_size,
+    border_type,
+    border_color,
+
+    text_overflow
+};
---
+++ include/quartz/ui/style.hpp
@@ -0,0 +1,39 @@
+#pragma once
+// C++ standard libraries
+#include <variant>
+#include <unordered_map>
+#include <stdexcept>
+
+// Own libraries
+#include "quartz/ui/properties.hpp"
+
+class Style {
+    using StyleValue = std::variant<std::monostate, int>;
+
+    public:
+        void Set(Properties property, StyleValue value) {
+            mData[property] = value;
+        }
+        
+        inline bool Has(Properties property) const {
+            auto it = mData.find(property);
+            if(it == mData.end()) {
+                return false;
+            }
+
+            return !std::holds_alternative<std::monostate>(it->second);
+        }
+
+        template<typename Type>
+        Type Get(Properties property) {
+            auto it = mData.find(property);
+            if(it == mData.end()) {
+                throw std::out_of_range("Property not found");
+            }
+
+            return std::get<Type>(it->second);
+        }
+
+    private:
+        std::unordered_map<Properties, StyleValue> mData;
+};
