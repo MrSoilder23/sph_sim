5ea2a260e260b4295f8391b859ea282c4606d09d
Filip <filip.makos49@gmail.com>
2025-07-03 02:03:21 +0200

    fix: changed to right output

--- shaders/gui_vert.glsl
+++ shaders/gui_vert.glsl
@@ -1,9 +1,9 @@
 #version 450 core
 
-layout(location = 0) position;
+layout(location = 0) in vec3 position;
 
 uniform mat4 uProjectionMatrix;
 
 void main() {
-    gl_PointSize = uProjectionMatrix * vec4(position, 1.0f);
+    gl_Position = uProjectionMatrix * vec4(position, 1.0f);
 }
