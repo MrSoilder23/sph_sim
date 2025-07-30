25576ca60c947e7153752ad27af301bb87de2cea
Filip <filip.makos49@gmail.com>
2025-07-31 01:06:03 +0200

    feat: new value struct for units

--- include/quartz/ui/utils/units.hpp
+++ include/quartz/ui/utils/units.hpp
@@ -1,10 +1,20 @@
 #pragma once
+// C++ standard libraries
+#include <variant>
 
 namespace quartz {
 
-enum class Units {
-    pixels, 
-    percentage
+enum class Unit {
+    Pixels, 
+    Percent,
+    Auto
 };
 
+struct Value {
+    std::variant<float, unsigned int> value;
+    Unit unit = Unit::Pixels;
+
+    Value(unsigned int value = 0, Unit unit = Unit::Pixels) : value(value), unit(unit) {}
+    Value(float value, Unit unit = Unit::Percent) : value(value), unit(unit) {}
+};
 }
